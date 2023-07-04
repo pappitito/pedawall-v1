25552bac6ac02a49be1ce31a88f40e7f744441f9
Tito <107757681+pappitito@users.noreply.github.com>
2023-07-04 22:27:32 +0100

    updates to sign up UI

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 }
 const Button = ({content, disabled}: ButtonProps) => {
   return (
-    <button className='w-full bg-[#E50914] rounded-[0.64rem] p-3 flex justify-center text-[white]'>{content}</button>
+    <button disabled={disabled} className={`w-full cursor-pointer trans ${disabled? 'bg-[#9f9e9e] cursor-not-allowed': 'bg-[#E50914]'} rounded-[0.64rem] p-3 flex justify-center text-[white]`}>{content}</button>
   )
 }
 
