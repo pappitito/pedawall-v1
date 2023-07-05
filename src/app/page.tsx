f327efcfb1194fd20f71479ae07cc912fa91cdfa
Tito <107757681+pappitito@users.noreply.github.com>
2023-07-05 01:00:41 +0100

    added login UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      
+      <div className='flex flex-col p-3 gap-3'>
+      <Link className='redlink ' href='/signup'>Sign up</Link>
+      <Link className='redlink ' href='/login'>Log in</Link>
+      </div>
     </main>
   )
 }
