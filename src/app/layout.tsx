639c7cae6878cf2e143bb40ae55d813404071282
Tito <107757681+pappitito@users.noreply.github.com>
2023-07-05 01:08:35 +0100

    maximum scale update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+      <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1" />
+
       </Head>
       <body className={poppins.className}>{children}</body>
     </html>
