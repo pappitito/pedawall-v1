6155348d5e41e68479e11f9d3a9532734e9f747d
Tito <107757681+pappitito@users.noreply.github.com>
2023-08-27 19:52:32 +0100

    change of images and explore with cart page added

---
+++ src/redux/slices/actuatorSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState ={
+
+}
+
+export const actuatorSlice = createSlice({
+    name: 'actuator',
+    initialState,
+    reducers: {
+        
+    }
+})
---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,18 @@
+import { CourseProp } from "@/components/misc";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserProps {
+    
+}
+
+const initialState: UserProps ={
+    
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        
+    }
+})
