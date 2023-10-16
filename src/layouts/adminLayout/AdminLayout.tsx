da0635d109984bf61ef1ba7f488d1ad6022d5fac
Tito <107757681+pappitito@users.noreply.github.com>
2023-10-16 23:30:03 +0100

    heade bg changed

--- src/layouts/adminLayout/AdminLayout.tsx
+++ src/layouts/adminLayout/AdminLayout.tsx
@@ -9,7 +9,7 @@ const AdminLayout: React.FC<LayoutProps> = ({children}) => {
     <div className='flex flex-col w-full'>
       <AdminHeader/>
       
-      <div className='flex w-full p-4 pl-0 pt-0 md:pt-4 md:pl-6 md:pr-6 flex-row'>
+      <div className='flex w-full p-4 pl-0 pt-0 md:pt-1 md:pl-6 md:pr-6 flex-row'>
         <div className='hidden sm:flex w-full max-w-[14rem] md:max-w-[16.4rem] '><AdminSidebar/></div>
         <div className='p-4 w-full'>{children}</div>
       </div>
